1fad6c31cf3f10860fe65b246caf7ab6d574d660
Alex Cantarero Fuentes <alex.cantarero@estudiantat.upc.edu>
2025-12-22 12:15:22 +0100

    /update: upload environment.ts

---
+++ Compare/src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  amadeusApiKey: 'WdLayBkAhg4xswxWf8o9Rfhsg95AkhyR',
+  amadeusApiSecret: '2ME8D2rFC34aaAWo',
+  amadeusUrl: 'https://test.api.amadeus.com/v1',
+  amadeusBearerToken: 'Aq2cOuo6YRLFA1cLaoYjrIrXytIr',
+  geminiApiKey: 'AIzaSyA8DCsNl51y9948nOZ7iYP4eKaBiXtjK7Q'
+};
